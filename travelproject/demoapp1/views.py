d05f878fd56eebbba6469a9e3c370a05b0857bd0
hemanthhbc <hemanthbchandran29@gmail.com>
2022-12-21 20:50:37 +0530

    added

---
+++ travelproject/demoapp1/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place
+from . models import Person
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1=Person.objects.all()
+    return render(request,"index.html",{'result':obj,'face':obj1})
